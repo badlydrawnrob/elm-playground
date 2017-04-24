a96d37c6d6faaee175f337148e96380661514cab
Rob <rob@layermade.com>
2017-04-24 19:02:35 +0200

    Testing immutability with javascript and elm, added directory to json

--- beginning-elm/elm-examples/Immutibility02.elm
+++ beginning-elm/elm-examples/Immutibility02.elm
@@ -1,5 +1,7 @@
 module Immutibility02 exposing (..)
 
+import Html
+
 
 multiplier =
     2
@@ -9,9 +11,31 @@ scores =
     [ 316, 320, 312, 370, 337, 318, 314 ]
 
 
-multiplier =
-    3
+
+-- Elm won't allow us to redefine `multiplier`!
+-- multiplier =
+--     3
 
 
 doubleScores scores =
     List.map (\x -> x * multiplier) scores
+
+s
+scoresLessThan320 scores =
+    List.filter isLessThan320 scores
+
+
+isLessThan320 score =
+    score < 320
+
+
+main =
+    doubleScores scores
+        |> toString
+        |> Html.text
+
+
+
+-- scoresLessThan320
+--     |> toString
+--     |> Html.text
--- beginning-elm/elm-examples/Immutibility02.js
+++ beginning-elm/elm-examples/Immutibility02.js
@@ -1,6 +1,7 @@
 var multiplier = 2;
 var scores = [316,320,370,337,318,314];
 
+// This mutates `multiplier` to 3:
 var multiplier = 3
 
 function doubleScores(scores) {
@@ -9,4 +10,29 @@ function doubleScores(scores) {
   }
 
   return scores;
+}
+
+function scoresLessThan320(scores) {
+  return scores.filter(isLessThan320)
+}
+
+function isLessThan320(score) {
+  return score < 320;
+}
+
+// The correct way to handle this in JS
+
+var multiplierCorrect = 2;
+var scoresCorrect = [316,320,370,337,318,314];
+
+function doubleScoresCorrect(scores) {
+  // Store the results in a new List
+  var newScores = [];
+
+  for (var i = 0; i < scores.length; i++) {
+    // Append to new list, no mutation
+    newScores[i] = scores[i] * multiplierCorrect;
+  }
+
+  return newScores;
 }
