f12e6087c5757c415f9253ec4441e5d726cf088e
Rob <rob@layermade.com>
2017-05-05 19:46:36 +0200

    Trying out a few test comparison functions with elm-test

--- beginning-elm/tests/RippleCarryAdderTests.elm
+++ beginning-elm/tests/RippleCarryAdderTests.elm
@@ -8,9 +8,28 @@ import RippleCarryAdder exposing (..)
 
 main =
     run <|
-        describe "Less than comparison"
-            [ test "an empty list's length is less than 1" <|
+        describe "Comparison"
+            [ test "2 is not equal to 3" <|
                 \() ->
-                    List.length []
-                        |> Expect.lessThan -1
+                    2 |> Expect.notEqual 3
+            , test "4 is less than 5" <|
+                \() ->
+                    4 |> Expect.lessThan 5
+            , test "6 is less than or equal to 7" <|
+                \() ->
+                    6 |> Expect.atMost 7
+            , test "9 is greater than 8" <|
+                \() ->
+                    9 |> Expect.greaterThan 8
+            , test "11 is greater than or equal to 10" <|
+                \() ->
+                    11 |> Expect.atLeast 10
+            , test "a list with zero elements is empty" <|
+                \() ->
+                    (List.isEmpty [])
+                        |> Expect.true "expected the list to be empty"
+            , test "a list with some elements is not empty" <|
+                \() ->
+                    (List.isEmpty [ "Jyn", "Cassian", "K-2SO" ])
+                        |> Expect.false "expected the list not to be empty"
             ]
