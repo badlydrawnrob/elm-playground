691fb7805a43073ac9a984dc1efcb31b35e90eaf
Rob <rob@layermade.com>
2017-07-06 17:40:14 +0200

    Update the Signup function

--- beginning-elm/elm-examples/Signup.elm
+++ beginning-elm/elm-examples/Signup.elm
@@ -2,6 +2,7 @@ module Signup exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 
 
 type alias User =
@@ -29,7 +30,7 @@ initialModel =
 -}
 
 
-view : User -> Html msg
+view : User -> Html Msg
 view user =
     div []
         [ h1 [] [ text "Sign up" ]
@@ -39,6 +40,7 @@ view user =
                 , input
                     [ id "name"
                     , type_ "text"
+                    , onInput SaveName
                     ]
                     []
                 ]
@@ -47,6 +49,7 @@ view user =
                 , input
                     [ id "email"
                     , type_ "email"
+                    , onInput SaveEmail
                     ]
                     []
                 ]
@@ -55,24 +58,51 @@ view user =
                 , input
                     [ id "password"
                     , type_ "password"
+                    , onInput SavePassword
                     ]
                     []
                 ]
             , div []
                 [ button
-                    [ type_ "submit" ]
+                    [ type_ "submit"
+                    , onClick Signup
+                    ]
                     [ text "Create my account" ]
                 ]
             ]
         ]
 
 
-update : msg -> User -> User
-update msg model =
-    initialModel
+type Msg
+    = SaveName String
+    | SaveEmail String
+    | Save Password String
+    | Signup
 
 
-main : Program Never User msg
+
+{-
+   If a pattern does exist, `update` executes the code for that pattern. Otherwise, the application crashes. Therefore, it’s important to handle all possible incoming messages.
+-}
+
+
+update : Msg -> User -> User
+update message model =
+    case message of
+        SaveName name ->
+            { user | name = name }
+
+        SaveEmail ->
+            { user | email = email }
+
+        SavePassword password ->
+            { user | password = password }
+
+        Signup ->
+            { user | loggedIn = True }
+
+
+main : Program Never User Msg
 main =
     beginnerProgram
         { model = initialModel
