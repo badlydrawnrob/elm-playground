6c1892521ed8005e9dab45bf508b3d1dd05b14b4
Rob <hello@robmcdonald.me>
2024-01-07 21:58:53 +0000

    Refactor the code
    1. Create a constant
    2. Create a helper function for `img`
    3. Remove `img` list from the `view`

--- elm-in-action/02/src/PhotoGroove.elm
+++ elm-in-action/02/src/PhotoGroove.elm
@@ -4,11 +4,10 @@ module PhotoGroove exposing (main)
     Photo Groove!
 
     : #1 Declares a new module
-    : #2 Imports other modules
+    : #2 Imports module's elements
     : #3 h1 element with an empty attributes list
     : #4 Put commas at the start of the line.
     : #5 img element with an empty children list
-    : #6 We’ll discuss “main” later.
 
     We're exposing `main` but NOT `view` for other modules to import.
     Another module that imported `PhotoGroove` would get
@@ -24,17 +23,29 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 
 
+-- Constants ------------------------------------------------------------
+-- : #1 Split out reusable stuff into separate constants
+urlPrefix =
+  "http://elm-in-action.com"
+
+
+-- View ------------------------------------------------------------------------
+-- This should base it's return value on the model argument
 view model =
   div [ class "content" ]
     [ h1 [] [ text "Photo Groove" ]  -- #3
-    , div [ id "thumbnails" ]        -- #4
-      [ img [ src "http://elm-in-action.com/1.jpeg" ] []  -- #5
-      , img [ src "http://elm-in-action.com/2.jpeg" ] []
-      , img [ src "http://elm-in-action.com/3.jpeg" ] []
-      ]
+    , div [ id "thumbnails" ] []     -- #4
     ]
 
--- Model --
+
+-- Helper functions ------------------------------------------------------------
+-- Helps another function to do it's job. Used in the `view` function
+-- where we've pulled out the image to use in the `view->img` list.
+viewThumbnail thumb =
+  img [ src (urlPrefix ++ thumb.url) ] []  -- #5: `++` concatonates strings
+
+
+-- Model -----------------------------------------------------------------------
 -- We create a list of _records_
 -- each record containing a `url` string
 initialModel =
