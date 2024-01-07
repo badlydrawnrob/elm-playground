0371637d409afb0c67a24f1d0cdb3d8e3f7fe84f
Rob <hello@robmcdonald.me>
2024-01-07 22:15:08 +0000

    Update the view function
    As it stands, this will throw an error:
    
    ```
    The 2nd argument to `div` is not what I expect:
    
    This `map` value is a:
    
        (a -> b) -> List a -> List b
    
    But `div` needs the 2nd argument to be:
    
        List (Html msg)
    ```
    
    The Html element `div` is expecting a `[]` list!!

--- elm-in-action/02/src/PhotoGroove.elm
+++ elm-in-action/02/src/PhotoGroove.elm
@@ -6,8 +6,6 @@ module PhotoGroove exposing (main)
     : #1 Declares a new module
     : #2 Imports module's elements
     : #3 h1 element with an empty attributes list
-    : #4 Put commas at the start of the line.
-    : #5 img element with an empty children list
 
     We're exposing `main` but NOT `view` for other modules to import.
     Another module that imported `PhotoGroove` would get
@@ -31,16 +29,34 @@ urlPrefix =
 
 -- View ------------------------------------------------------------------------
 -- This should base it's return value on the model argument
+--
+-- : #4 Because each Html element accepts two lists:
+--      - An attributes list
+--      - A child list
+--      we can use `List.map` function to return a list!
+--
+-- : #5 We're passing `viewThumbnail` as the 1st argument to our _higher order
+--      function_ `List.map` along with the `model` list of `url string` records.
+--      - Could look better wrapped in parens so it looks more like a function,
+--        and not an argument
 view model =
   div [ class "content" ]
     [ h1 [] [ text "Photo Groove" ]  -- #3
-    , div [ id "thumbnails" ] []     -- #4
+    , div [ id "thumbnails" ] List.map viewThumbnail model     -- #4
     ]
 
+
+-- See `Figure 2.8`
+-- : #1 We pass List.map a translation function and a list
+-- : #2 It runs that translation function on each value in the list.
+-- : #3 List.map returns a new list containing the translated values.
 
 -- Helper functions ------------------------------------------------------------
 -- Helps another function to do it's job. Used in the `view` function
 -- where we've pulled out the image to use in the `view->img` list.
+--
+-- : #5 We've moved our list of `img` to a single function,
+--      which is a single Html `img` element. We pass this function to `List.map`
 viewThumbnail thumb =
   img [ src (urlPrefix ++ thumb.url) ] []  -- #5: `++` concatonates strings
 
@@ -49,7 +65,7 @@ viewThumbnail thumb =
 -- We create a list of _records_
 -- each record containing a `url` string
 initialModel =
-  [ { url = "1.jpeg" }
+  [ { url = "1.jpeg" }  -- #4
   , { url = "2.jpeg" }
   , { url = "3.jpeg" }
   ]
