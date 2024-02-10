974a305c5d6a559d5bcf7e1750bf01c1954c0fb8
Rob <hello@robmcdonald.me>
2024-02-10 21:43:18 +0000

    Add some examples of decoders
    A new `notes` file.

--- elm-in-action/04/tests/http-get-json/src/HttpExamples.elm
+++ elm-in-action/04/tests/http-get-json/src/HttpExamples.elm
@@ -4,6 +4,7 @@ import Html exposing (..)
 import Html.Events exposing (onClick)
 import Http
 import Browser
+import Json.Decode exposing (Decoder, Error(...), decodeString, list, string)
 
 
 -- Model -----------------------------------------------------------------------
@@ -65,7 +66,44 @@ viewNickname nickname =
 
 -- Update ----------------------------------------------------------------------
 
--- #1 `Http.get` type signature looks like this:
+-- #1  Elm has a Type called `Result`. It accepts two arguments,
+--     in containers `Ok value` or `Err error` type variants.
+--
+--     : Our `Msg` for Random is simpler, because it always succeeds.
+--       Fetching data from a server can fail. Perhaps the server isn’t
+--       available or the URL we’re trying to reach is incorrect.
+--
+--
+-- #2  `Http.Error` is a built-in custom type!
+--
+--      type Error
+--        = BadUrl String
+--        | Timeout
+--        | NetworkError
+--        | BadStatus Int
+--        | BadBody String
+--
+--
+-- #3  Errors ... try loading with an incorrect `url` for example:
+--     @ http://localhost:5016/invalid.txt
+--
+--     : We use a function called `buildErrorMessage` to `case` on all possible
+--       `Http.Error`s and return a String for each one.
+--
+--     : We wrap the result in a `Just String` which we'll use in the `view`!
+--       We `case` again with a `viewNicknamesOrError` helper function, and
+--       pass forward the data that we need.
+--
+--       - `viewError String` or `viewNicknames List String`.
+--
+--
+-- #4 `Http.expectString` looks like this:
+--     `expectString : (Result Http.Error String -> msg) -> Expect msg
+--
+--     : Elm Runtime will send a `Msg` to update for us.
+--
+--
+-- #5 `Http.get` type signature looks like this:
 --     `get : { url : String, expect : Expect msg } -> Cmd Msg`
 --
 --     : It takes a record with two fields and returns a Cmd
@@ -77,63 +115,61 @@ viewNickname nickname =
 --     3. Which message should be sent to the app after the
 --        command has been executed?
 --
--- #2 `Http.expectString` looks like this:
---     `expectString : (Result Http.Error String -> msg) -> Expect msg
 --
---     : Elm Runtime will send a `Msg` to update for us.
---
--- #3  Elm has a Type called `Result`. It accepts two arguments,
---     in containers `Ok value` or `Err error` type variants.
---
---     : Our `Msg` for Random is simpler, because it always succeeds.
---       Fetching data from a server can fail. Perhaps the server isn’t
---       available or the URL we’re trying to reach is incorrect.
---
--- #4  `Http.Error` is a built-in custom type!
---
---      type Error
---        = BadUrl String
---        | Timeout
---        | NetworkError
---        | BadStatus Int
---        | BadBody String
---
--- #5  We need to tell the `update` function what to do when the
+-- #6  We need to tell the `update` function what to do when the
 --     `DataReceived` message arrives! All we are doing here is
 --     unpacking the result payload that rides on `DataReceived`’s back.
 --
 --     : We can use pattern matching instead of nested `case`.
 --       @ http://tinyurl.com/beginning-elm-pattern-matching
 --
---     : We’ve also replaced the payload httpError with `_`
---       because we aren’t using it right now.
 --
--- #6  Errors ... try loading with an incorrect `url` for example:
---     @ http://localhost:5016/invalid.txt
+-- #7  Our decoders can be an `atom`, or a `List atom`. This generates
+--     a `Decoder` that knows how to translate a JSON array into a list
+--     of Elm Strings.
 --
---     : We use a function called `buildErrorMessage` to `case` on all possible
---       `Http.Error`s and return a String for each one.
+--     `list` is a function that returns a decoder:
+--         `Decoder a -> Decoder (List a)`
+--     `string` returns a Decoder:
+--         `Decoder string`
+--      `list string` returns a Decoder:
+--          `Decoder (List String)`
 --
---     : We wrap the result in a `Just String` which we'll use in the `view`!
---       We `case` again with a `viewNicknamesOrError` helper function, and
---       pass forward the data that we need.
+--     : These by themselves DO NOT DECODE JSON. It's like a recipe, or
+--       instructions to be passed on to a function like `decodeString`,
+--       which does the actual decoding. That first parses a raw string
+--       into JSON and applies the Decoder to it.
 --
---       - `viewError String` or `viewNicknames List String`.
+--       `decodeString string "\"Beanie\""` -> Ok "Beanie"
+--       `decodeString string "42"          -> Error (Failure ...)
+--
+--        @ http://tinyurl.com/elm-lang-json-decode-string
+--        @ http://tinyurl.com/elm-lang-json-decode-error
+--
+--      : You can create larger decoders by using primitave decoders and
+--        using them as building blocks.
+--
+
 
 type Msg
   = SendHttpRequest
-  | DataReceived (Result Http.Error String)  -- #3, #4
+  | DataReceived (Result Http.Error String)  -- #1, #2
 
 url : String
 url =
-  "http://localhost:5016/nicknames"  -- #6
+  "http://localhost:5016/nicknames"  -- #3
 
 getNicknames : Cmd Msg
 getNicknames =
-  Http.get  -- #1
+  Http.get  -- #5
     { url = url
-    , expect = Http.expectString DataReceived  -- #2
+    , expect = Http.expectString DataReceived  -- #4
     }
+
+
+nicknamesDecoder : Decoder (List String)
+nicknamesDecoder =
+  list string  -- #7
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
@@ -141,14 +177,14 @@ update msg model =
     SendHttpRequest ->
       ( model, getNicknames )
 
-    DataReceived (Ok nicknamesStr) ->  -- #5
+    DataReceived (Ok nicknamesStr) ->  -- #1, #6
       let
         nicknames =
           String.split "," nicknamesStr
       in
       ( { model |  nicknames = nicknames }, Cmd.none )
     DataReceived (Err error) ->
-      ( { model | errorMessage = Just (buildErrorMessage error) }  -- #6
+      ( { model | errorMessage = Just (buildErrorMessage error) }  -- #2
       , Cmd.none
       )
 
