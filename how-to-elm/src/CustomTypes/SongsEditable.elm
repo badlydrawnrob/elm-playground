6ff959013921d9697c6fb27b6190ae3d6c06346f
Rob <hello@robmcdonald.me>
2024-10-20 20:59:45 +0100

    Initial model for multiple Albums
    I need to consider how to set up the model to allow for various states:
    
    1. New Album
    2. Edit Album
    3. Delete Album
    
    The same goes for Songs, although I might remove the necessity for having a song ID.

--- how-to-elm/src/CustomTypes/SongsEditable.elm
+++ how-to-elm/src/CustomTypes/SongsEditable.elm
@@ -4,24 +4,38 @@ module CustomTypes.SongsEditable exposing (..)
     Creating A Simple Custom Type (see `CustomTypes.md` for notes)
     ============================================================================
     This started out as an alternative for a `Maybe List`. Remember that the
-    simplest thing possible is often the best solution. If you don't need complexity
-    then don't add it.
+    simplest thing possible is often the best solution. If you don't need
+    complexity — don't add it!
 
-    Options for our `Model` (nested record/custom type)
-        @ Songs.elm  -OR-  @ SongsAlt.elm
+    We have a couple of options for our model:
+    ------------------------------------------
+    For now I'm sticking with the nested record approach
+
+        @ Songs.elm    (nested record)
+        @ SongsAlt.elm (custom `UserInput` type)
 
     Learning points
     ---------------
+    I'm using a record here for `UserInput` rather than a custom type. The benefit
+    of a custom type is that it's FLATTER (no cranky nested record update function),
+    but the benefit of a record is it's accessor `.valid` `.input` etc. They almost
+    balance each other out, so it's a matter of preference.
+
+    @ https://www.diffchecker.com/me6aANHb/
+
     1. `Msg` is for CARRYING DATA and NOT for changing state
-        @ https://discourse.elm-lang.org/t/message-types-carrying-new-state/2177/5
+        - @ https://discourse.elm-lang.org/t/message-types-carrying-new-state/2177/5
     2. Lift `Maybe` in ONE place when possible
     3. Look how `Song` is created; `updateAlbum` is simply passed a `Song`.
-    4. A `Tuple` adds some complexity. Don't bother? (definitely not for user input)
+    4. A `Tuple` adds some complexity.
+        - UserInput is way easier _without_ a `Tuple`.
     5. Remember the roles of `Msg`, `Update`, `View` and put functions in right place!
     6. Validating forms is a bit of a minefield. Just get something working.
-        - There's many ways to do it.
+        - There's many ways to do it
+        - @ https://tinyurl.com/the-elm-way-to-validate-form
     7. Remember the `2.0` fiasco. Simplify data entry at source.
-    8. Also simplify your state wherever possible
+        - The fewer state possibilities, the better (in general)
+    8. Simplify your state wherever possible:
         - See "The 5 ways to reduce code"
     9. Nested records are OK in moderation, but prefer a flatter style ...
         - You could easily just write a big record with more fields.
@@ -31,10 +45,20 @@ module CustomTypes.SongsEditable exposing (..)
         - @ https://tinyurl.com/elm-lang-why-not-nested
         - @ https://tinyurl.com/elm-spa-custom-types-eg
     10. Chaining `Result`s seems like a bad idea (see `HowToResult/FieldError`)
-        - Should probably only use `Result.andThen` for a single data point.
+        - Only use `Result.andThen` for a single data point (it seems to me)
     11. The program (and data) FLOW should be as SIMPLE as possible:
         - Can you re-read it and tell EXACTLY what's going on?
-        - I certainly can't do that quickly with `HowToResult/FieldError`)
+        - I certainly can't do that quickly with `HowToResult/FieldError`
+
+
+    Questions
+    ---------
+    1. I could've used `Album { id = ..., song = Song }` instead of params.
+       - What's the benefit of using a record in a custom type over arguments?
+       - I've heard that a function should have as few parameters as possible.
+       - I also understand the benefit of typed stuff like `FirstName String`.
+    2. Can I create just ONE `getID` for `SongID` and `AlbumID`?
+    3. How do you `.map` over a bunch of user inputs if they're different types?
 
     ----------------------------------------------------------------------------
     Wishlist
@@ -63,3 +87,79 @@ module CustomTypes.SongsEditable exposing (..)
         3. Splitting out components/messages/etc
         4. Only ONE `Song`?
 -}
+
+import Url exposing (Url, fromString)
+
+
+-- Model -----------------------------------------------------------------------
+-- #! Aim for the simplest thing possible that could work BEFORE thinking about
+--    more complicated setup (which will be needed eventually)
+--
+--    Album could be one of NewAlbum, EditAlbum, DeleteAlbum.
+--    Song could be one of NewSong, EditSong, DeleteSong.
+--    we also need to load it from `json` and update it by `json`.
+--    we might eventually need a `Status` (loading/loaded/failed/etc)
+--    should I just generate a `uuid` for album/song?
+--
+--    You also have the option of referencing a song by NAME or ID.
+--    It might not be essential for each song to have it's own ID.
+--    Check if name already exists in (List Song) as protection!
+
+type AlbumID =
+    AlbumID Int
+
+type alias AlbumName =
+    String
+
+getAlbumID : AlbumID -> Int
+getAlbumID (AlbumID id) =
+    id
+
+type Album =
+    Album AlbumID AlbumName AlbumSongs
+
+type AlbumSongs =
+    AlbumSongs Song (List Song)
+
+type SongID =
+    SongID Int
+
+getSongID : SongID -> Int
+getSongID (SongID id) =
+    id
+
+type alias RunTime =
+    (Int, Int)
+
+type alias Song
+    = { id : SongID
+      , title : String
+      , time  : RunTime
+      , url : Maybe Url  -- A YouTube link, for instance
+      }
+
+type alias Validate a
+    = Result String a
+
+{- This could've been a custom type -}
+type alias UserInput a
+    = { input : String
+      , valid : Validate a
+      }
+
+initUserInput = { input = "", valid = Err "Field cannot be empty" }
+
+{- #! I need to handle the AlbumID and the SongID -}
+type alias Model =
+    { albumID : AlbumID
+    , albumName : AblumName
+    , songID : SongID
+    , songName : UserInput String
+    , minutes : UserInput Int
+    , seconds : UserInput Int
+    , albums : List Album
+    }
+
+
+
+-- Update ----------------------------------------------------------------------
