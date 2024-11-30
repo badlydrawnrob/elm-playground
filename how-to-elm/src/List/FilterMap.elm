3a4cf699a1665fcd533c866c4ca35d6ee94edfbb
Rob <hello@robmcdonald.me>
2024-11-30 15:57:59 +0000

    Add  a 2x lambda example

--- how-to-elm/src/List/FilterMap.elm
+++ how-to-elm/src/List/FilterMap.elm
@@ -6,7 +6,14 @@ module List.FilterMap exposing (..)
     Requires a function that converts a `value` to a `Maybe value`.
         @ https://guide.elm-lang.org/error_handling/maybe
 
+    Lessons learned
+    ---------------
+    1. Careful with types: `(a -> Maybe a)` should be `b`
+    2. Careful with types: `List a` should contain `List (List a)`
+    3. `String.startsWith` is case-sensitive!
+
 -}
+
 import Html exposing (a)
 
 -- Simple `a -> b` examples ----------------------------------------------------
@@ -33,3 +40,12 @@ filterByLambda = List.filterMap
                             else
                                 Nothing)
                     ["sad song", "happy song", "happy life"]
+
+{- Function is case-sensitive -}
+filterByLambdaX2 : List String
+filterByLambdaX2 = List.filterMap
+                    (\str -> if (String.startsWith "W" str) && (String.endsWith "s" str) then
+                                Just str
+                            else
+                                Nothing)
+                    ["Weekends", "what a fucking day", "wet weather is", "Words"]
