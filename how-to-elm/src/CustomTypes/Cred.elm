88e0b6e65057fe161bc17c2b285b224ff10778c4
Rob <hello@robmcdonald.me>
2024-12-30 20:24:19 +0000

    Create How To for `Cred`
    Using the lesson from Frontend Masters, create a couple of modules that use Opaque Types

---
+++ how-to-elm/src/CustomTypes/Cred.elm
@@ -0,0 +1,61 @@
+module CustomTypes.Cred exposing (Cred, decoder, encodeToken, username)
+
+{-| ----------------------------------------------------------------------------
+    Simple `Cred`entials with `Username` custom type
+    ============================================================================
+    ⚠️ Remember, `Cred` is an OPAQUE TYPE. You have to expose the functions that
+    create and transform this type. `Username` is also an opaque type. See the module's `toString` function, as
+    well as it's `decoder`.
+
+    The only way to create these opaque types is when decoding from `json`.
+
+    What I learned
+    --------------
+    1. To access the `username` function, we could import as
+       `Cred` (the module) and use `Cred.username` to get the function. We can
+       then pass `username` the `Cred` (decoded custom type) value.
+    2. Using a record isn't necessary for so few values
+    3. But a record can be deconstructed (Cred { token }) without `_`, which
+       makes life a little easier.
+    4. Don't fall into the trap of making too many "getters" or "setters".
+        - The whole point of Opaque Types is to hide implementation detail.
+        - Only expose what's absolutely essential (such as `Username`)
+
+    Gotchas
+    -------
+    > Username is an OPAQUE TYPE. It doesn't expose it's externals.
+    > This means `String` is only accessible with the `toString` function.
+
+    To get the username `String`, you'll need to first pull out the
+    `Username` value from `Cred`, then use `Username.toString` from
+    the `Username` module!
+
+-}
+
+import CustomTypes.Username exposing (Username)
+import Json.Encode as Encode exposing (Value)
+import Json.Decode as Decode exposing (Decoder)
+import Json.Decode.Pipeline exposing (required)
+import CustomTypes.Username as Username
+
+type Cred
+    = Cred Username String
+
+username : Cred -> Username
+username (Cred username _) =
+    username -- See `Username` module
+
+
+-- Decoders --------------------------------------------------------------------
+
+decoder : Decoder Cred
+decoder =
+    Decode.succed Cred
+        |> required "username" Username.decoder
+        |> required "token" Decode.string
+
+-- Encoders --------------------------------------------------------------------
+
+encodeToken : Cred -> Value
+encodeToken (Cred _ token) =
+    Encode.string token
---
+++ how-to-elm/src/CustomTypes/Username.elm
@@ -0,0 +1,25 @@
+module CustomTypes.Username exposing (Username, decoder, toString)
+
+{-| ----------------------------------------------------------------------------
+    A Simple `Username` type (@imported by `Cred.elm`)
+    ============================================================================
+    ⚠️ Remember, `Username` is an OPAQUE TYPE and doesn't expose it's internals.
+    The ONLY way to get a `Username` is to decode one from `json`. To get the
+    `String`, we can expose the `toString` function, once we've decoded.
+
+    It's quite nice to have each key element with it's own decoder and `toString`
+    function, for ease of naming conventions.
+-}
+
+import Json.Decode as albumDecoder exposing (Decoder)
+
+type Username
+    = Username String
+
+decoder : Decoder Username
+decoder =
+    Decode.map Username Decode.string
+
+toString : Username -> String
+toString (Username username) =
+    username
