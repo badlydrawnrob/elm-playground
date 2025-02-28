f433d6f943b05c7b8f30d435222541710adfa08f
Rob <hello@robmcdonald.me>
2025-02-28 21:21:37 +0000

    Working with ports
    1. Local storage
    2. Settings mini-model
    
    Man, after 1 month without coding any Elm you forget a lot (even when using Anki flashcards!)

--- how-to-elm/README.md
+++ how-to-elm/README.md
@@ -1,3 +1,20 @@
 # README
 
-> I'm beginning to think maybe it's a good thing that Elm forces you to build certain things WITHOUT packages or dependencies. NPM is a shit-show. A book I've been reading has `100 vulnerabilities` and outdated packages. No way in hell I'm sorting through that lot.
+> The good bits of Elm
+
+1. It forces you to build things without too many dependencies
+2. Some low-level detail must be done by the user (js makes you a bit lazy)
+
+These can be a blessing and a curse, as some things might be better with a dedicated package, such as image uploads. For that you have to understand what `base64` is and how file uploads work.
+
+
+## Rules
+
+> I need to start setting rules for myself
+
+1. What format am I using? (custom headings or `elm-format`)
+2. What indentation style am I preferring? (`2` or `4` spaces)
+3. What learning is "in" and what is "out"?[^1]
+
+
+[^1]: I'm using Elm as my go-to language and for that reason learning the details in a little more depth than, say, Python. However, my purpose is prototyping and I still want to keep my learning light. Some implementations (SPA, packages) get complicated quite quickly as an app scales.
---
+++ how-to-elm/src/Ports/LocalStorage.elm
@@ -0,0 +1,115 @@
+module Ports.LocalStorage exposing (main)
+
+{-| ----------------------------------------------------------------------------
+    LocalStorage example (for themes, cookies, and session)
+    ============================================================================
+    We can persist across our application by using LocalStorage. This will remain
+    on our user's device until they clear their browser's cache. See the original
+    examples from Elm Guide:
+        @ https://ellie-app.com/8yYddD6HRYJa1
+        @ https://guide.elm-lang.org/interop/ports.html
+
+    Notes
+    -----
+    1. I'm using @rtfeldman's "magic" nested settings state pattern.
+       @ https://tinyurl.com/elm-spa-login-update
+
+    Wishlist
+    --------
+    1. How do I "flush" the cache for new settings version?
+        - Resaving the file, or `?version`ing the import doesn't work.
+        - You could set a timer on the `localStorage`?
+
+-}
+
+import Browser
+import Html exposing (button, Html, main_, text)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+import Ports.Settings as P exposing (..)
+import Json.Decode as D
+import Json.Encode as E
+import CustomTypes.Songs exposing (Msg)
+
+
+-- Main ------------------------------------------------------------------------
+-- The second argument is the initial settings (flags)
+
+main : Program E.Value Model Msg
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = updateWithStorage
+        , subscriptions = \_ -> Sub.none
+        }
+
+init : E.Value -> ( Model, Cmd Msg )
+init flags =
+    (
+        case D.decodeValue P.decoder flags of
+            Ok settings ->
+                { settings = settings }
+            Err _ ->
+                { settings = { theme = Light, cookies = False, session = "logged in" } }
+    , Cmd.none
+    )
+
+
+-- Model -----------------------------------------------------------------------
+
+type alias Model =
+    { settings : P.Settings }
+
+
+-- View ------------------------------------------------------------------------
+
+view : Model -> Html Msg
+view model =
+    main_ []
+        [ viewThemeButton model.settings.theme
+        , button [ onClick HideCookies] [ text "Hide cookies" ]
+        , button [ onClick ClearSession] [ text "Clear session" ]
+        ]
+
+viewThemeButton : Theme -> Html Msg
+viewThemeButton theme =
+    button [ onClick (ToggleTheme (P.themeToggle theme)) ]
+        [ text (P.themeToString theme) ]
+
+-- Messages --------------------------------------------------------------------
+
+type Msg
+    = ToggleTheme Theme
+    | HideCookies
+    | ClearSession
+
+
+-- Update ----------------------------------------------------------------------
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        ToggleTheme theme ->
+            updateSettings (\settings -> { settings | theme = theme }) model
+
+        HideCookies ->
+            updateSettings (\settings -> { settings | cookies = True }) model
+
+        ClearSession ->
+            updateSettings (\settings -> { settings | session = "" }) model
+
+{- Helper function for update. Updates the settings -}
+updateSettings : (P.Settings -> P.Settings) -> Model -> ( Model, Cmd Msg )
+updateSettings transform model =
+    ( { model | settings = transform model.settings }, Cmd.none )
+
+updateWithStorage : Msg -> Model -> ( Model, Cmd Msg )
+updateWithStorage msg oldModel =
+    let
+        ( newModel, cmd ) = update msg oldModel
+    in
+    ( newModel,
+      P.setStorage (P.encoder newModel.settings) -- elm guide uses `Cmd.batch`
+    )
+
